a3caf55225706ce0469aeb18b48c85743d54754a
sarah <s.j.condon01@gmail.com>
2019-09-24 23:25:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = nil
   name_hash.each do |key,value|
-    if value < lowest_value  || lowest_value == nil
+    if lowest_value == nil ||value < lowest_value  || lowest_value == nil
      lowest_key = key
  end 
  end
